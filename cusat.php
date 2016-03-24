aa5a44763cfe7c44ea9549d2d472a2a253d4d0da
balumenon <balbbrl@gmail.com>
2016-03-24 22:56:09 +0530

    initial ush

---
+++ cusat.php
@@ -0,0 +1,73 @@
+<?php 
+// Create DOM from URL or file
+include('Simplehtmldom/simple_html_dom.php');
+require 'PHPMailer/PHPMailerAutoload.php';
+
+$html = file_get_html('http://exam.cusat.ac.in/');
+
+// Find all images 
+// foreach($html->find('img') as $element) 
+//        echo $element->src . '<br>';
+
+// Find all links 
+
+$con = mysqli_connect('localhost','root','','cusat') or die('Couldnt connect');
+
+
+foreach($html->find('a') as $element) {
+	//echo $element->href . '<br>';
+	$inner=strip_tags($element->innertext);
+	//sendMail('test');
+	$query = "INSERT INTO `innertext`(`inner`) VALUES ('$inner')";
+
+	mysqli_query($con,$query);
+
+	if (mysqli_affected_rows($con)==1) {
+		echo "<br> Inserted ".$inner;
+
+
+require_once "Mail.php";
+
+$from     = "cusatcrawler@gmail.com";
+$to       = "ajmalazeez007@gmail.com";
+$subject  = "Test email using PHP SMTP with SSL\r\n\r\n";
+$body     = "This is a test email message";
+$host     = "smtp.gmail.com";
+$port     = "587";
+$username = "cusatcrawler@gmail.com";
+$password = "asd123##";
+$headers  = array(
+    'From' => "cusatcrawler@gmail.com",
+    'To' => $to,
+    'Subject' => $subject
+);
+$smtp     = Mail::factory('smtp', array(
+    'host' => $host,
+    'port' => $port,
+    'auth' => true,
+    'username' => $username,
+    'password' => $password
+));
+$mail     = $smtp->send($to, $headers, $body);
+if (PEAR::isError($mail)) {
+    echo ("
+" . $mail->getMessage() . "
+
+");
+} else {
+    echo ("
+Message successfully sent!
+
+");
+}
+
+	}
+
+
+}
+  
+
+  
+       	
+          
+ ?>
