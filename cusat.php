77b944064402379e07d1b14d5ac4c50678fac94c
balumenon <balbbrl@gmail.com>
2016-03-24 23:15:20 +0530

    mail debug

--- cusat.php
+++ cusat.php
@@ -26,40 +26,38 @@ foreach($html->find('a') as $element) {
 		echo "<br> Inserted ".$inner;
 
 
-require_once "Mail.php";
+$Mail = new PHPMailer();
+  $Mail->IsSMTP(); // Use SMTP
+  
+  $Mail->Host        = "smtp.gmail.com"; // Sets SMTP server
+  $Mail->SMTPDebug   = 2; // 2 to enable SMTP debug information
+  $Mail->SMTPAuth    = TRUE; // enable SMTP authentication
+  $Mail->SMTPSecure  = "tsl"; //Secure conection
+  $Mail->Port        = 587; // set the SMTP port
+  $Mail->Username    = 'cusatcrawler@gmail.com'; // SMTP account username
+  $Mail->Password    = 'asd123##'; // SMTP account password
+  $Mail->Priority    = 1; // Highest priority - Email priority (1 = High, 3 = Normal, 5 = low)
+  $Mail->CharSet     = 'UTF-8';
+  $Mail->Encoding    = '8bit';
+  $Mail->Subject     = 'Test Email Using Gmail';
+  $Mail->ContentType = 'text/html; charset=utf-8\r\n';
+  $Mail->From        = 'cusatcrawler@gmail.com';
+  $Mail->FromName    = 'GMail Test';
+  $Mail->WordWrap    = 900; // RFC 2822 Compliant for Max 998 characters per line
 
-$from     = "cusatcrawler@gmail.com";
-$to       = "ajmalazeez007@gmail.com";
-$subject  = "Test email using PHP SMTP with SSL\r\n\r\n";
-$body     = "This is a test email message";
-$host     = "smtp.gmail.com";
-$port     = "465";
-$username = "cusatcrawler@gmail.com";
-$password = "asd123##";
-$headers  = array(
-    'From' => "cusatcrawler@gmail.com",
-    'To' => $to,
-    'Subject' => $subject
-);
-$smtp     = Mail::factory('smtp', array(
-    'host' => $host,
-    'port' => $port,
-    'auth' => true,
-    'username' => $username,
-    'password' => $password
-));
-$mail     = $smtp->send($to, $headers, $body);
-if (PEAR::isError($mail)) {
-    echo ("Error
-" . $mail->getMessage() . "
+  $Mail->AddAddress('ajmalazeez007@gmail.com'); // To:
+  $Mail->isHTML( TRUE );
+  $Mail->Body    = 'Email Body';
+  $Mail->AltBody = 'Alst Email Body';
+  $Mail->Send();
+  $Mail->SmtpClose();
 
-");
-} else {
-    echo ("
-Message successfully sent!
-
-");
-}
+  if ( $Mail->IsError() ) { // ADDED - This error checking was missing
+    echo "false";
+  }
+  else {
+    echo "true";
+  }
 
 	}
 
