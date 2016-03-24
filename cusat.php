7ee464efa54c3d34b4a7d4ddaae7c8da3db1a4bc
balumenon <balbbrl@gmail.com>
2016-03-25 00:09:13 +0530

    mail debug

--- cusat.php
+++ cusat.php
@@ -11,7 +11,7 @@ $html = file_get_html('http://exam.cusat.ac.in/');
 
 // Find all links 
 
-$con = mysqli_connect('localhost','root','','cusat') or die('Couldnt connect');
+$con = mysqli_connect('localhost','root','asd123##','cusat') or die('Couldnt connect');
 
 
 foreach($html->find('a') as $element) {
@@ -30,19 +30,19 @@ foreach($html->find('a') as $element) {
 $Mail = new PHPMailer();
   $Mail->IsSMTP(); // Use SMTP
   
-  $Mail->Host        = "smtp.gmail.com"; // Sets SMTP server
+  $Mail->Host        = "smtp-pulse.com"; // Sets SMTP server
   $Mail->SMTPDebug   = 2; // 2 to enable SMTP debug information
   $Mail->SMTPAuth    = FALSE; // enable SMTP authentication
   $Mail->SMTPSecure  = "ssl"; //Secure conection
   $Mail->Port        = 465; // set the SMTP port
-  $Mail->Username    = 'cusatcrawler@gmail.com'; // SMTP account username
-  $Mail->Password    = 'asd123##'; // SMTP account password
+  $Mail->Username    = 'ajmalazeez009@gmail.com'; // SMTP account username
+  $Mail->Password    = 'LAErtsYsQSGTN'; // SMTP account password
   $Mail->Priority    = 1; // Highest priority - Email priority (1 = High, 3 = Normal, 5 = low)
   $Mail->CharSet     = 'UTF-8';
   $Mail->Encoding    = '8bit';
   $Mail->Subject     = 'Test Email Using Gmail';
   $Mail->ContentType = 'text/html; charset=utf-8\r\n';
-  $Mail->From        = 'cusatcrawler@gmail.com';
+  $Mail->From        = 'ajmalazeez009@gmail.com';
   $Mail->FromName    = 'GMail Test';
   $Mail->WordWrap    = 900; // RFC 2822 Compliant for Max 998 characters per line
 
