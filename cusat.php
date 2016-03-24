7fa41a757065b41be1056a962eadbe26bad8b938
balumenon <balbbrl@gmail.com>
2016-03-24 23:07:10 +0530

    db changes

--- cusat.php
+++ cusat.php
@@ -11,7 +11,7 @@ $html = file_get_html('http://exam.cusat.ac.in/');
 
 // Find all links 
 
-$con = mysqli_connect('localhost','root','','cusat') or die('Couldnt connect');
+$con = mysqli_connect('localhost','root','asd123##','cusat') or die('Couldnt connect');
 
 
 foreach($html->find('a') as $element) {
